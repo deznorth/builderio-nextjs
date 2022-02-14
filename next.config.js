0d2a0a9e7e5bae67eff044b1fbded331c2fc0599
David Rojas <deznorth@gmail.com>
2022-02-14 01:55:18 -0500

    Add SASS and move things around

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
+  env: {
+    BUILDER_API_KEY: process.env.BUILDER_API_KEY,
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => ({
+    ...config,
+    resolve: {
+      ...config.resolve,
+      alias: {
+        // Configura aliases for easy module paths
+        ...config.resolve.alias,
+        components: path.resolve(__dirname, 'src', 'components'),
+        pages: path.resolve(__dirname, 'src', 'pages'),
+        public: path.resolve(__dirname, 'public'),
+        styles: path.resolve(__dirname, 'src', 'styles'),
+      },
+    },
+  }),
 }
